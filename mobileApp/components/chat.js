cd4f1772f2e5a7b48f45f45aaac96edfadd35f6e
Kevin T <kdt0116@tamu.edu>
2020-04-19 18:31:43 -0500

    added system message support

--- mobileApp/components/chat.js
+++ mobileApp/components/chat.js
@@ -1,90 +1,96 @@
-
-import { StyleSheet, Text, View, RecyclerViewBackedScrollViewComponent } from 'react-native';
-import { GiftedChat,Bubble  } from 'react-native-gifted-chat'
+import {
+  StyleSheet,
+  Text,
+  View,
+  RecyclerViewBackedScrollViewComponent,
+} from "react-native";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 import React, { useState, useEffect } from "react";
-import Rhino from '../assets/roomierhino.png'
-import { DbContext } from '../context';
+import Rhino from "../assets/roomierhino.png";
+import { DbContext } from "../context";
 
 // Required for websocket functionality
 import {
   getRoomMessages,
   getUserRoom,
   joinRoom,
-  createAndJoinRoom
+  createAndJoinRoom,
 } from "../src/request";
 import * as websocket from "../src/websocket";
 
-
-const parseMsg = msg => {
+const parseMsg = (msg) => {
   return {
     _id: msg.giftedId || msg._id,
     mongoId: msg._id,
     text: msg.data,
     createdAt: msg.createdAt || Date.now(),
     sent: true,
-    user:{
+    system: msg.system,
+    user: {
       _id: msg.senderId,
       name: msg.senderId,
-    }
-  }
-}
+    },
+  };
+};
 
-const formatMessage = msg => {
+const formatMessage = (msg) => {
   msg.pending = true;
   msg.createdAt = Date.parse(msg.createdAt);
   return msg;
-}
+};
 
 class Chat extends React.Component {
   static contextType = DbContext;
 
-  constructor(props)
-  {
+  constructor(props) {
     super(props);
-  
+
     this.state = {
-      messages:[]
-    }
+      messages: [],
+    };
   }
 
-  componentDidMount()
-  {
-    getRoomMessages(this.context.room).then(resMessages => {
-      this.setState({
-        messages: resMessages.map(parseMsg).reverse()
-      });
+  componentDidMount() {
+    getRoomMessages(this.context.room)
+      .then((resMessages) => {
+        this.setState({
+          messages: resMessages.map(parseMsg).reverse(),
+        });
 
-      if(websocket.getWebSocket()) {
-        websocket.getWebSocket().onmessage = msg =>{
-          const data = parseMsg(JSON.parse(msg.data));
+        if (websocket.getWebSocket()) {
+          websocket.getWebSocket().onmessage = (msg) => {
+            const data = parseMsg(JSON.parse(msg.data));
 
-          console.log("RECEIVED MESSAGE FROM WEBSOCKET");
+            console.log("RECEIVED MESSAGE FROM WEBSOCKET", data);
 
-          if (data.user._id == this.context.user._id) {
-            const sentIndex = this.state.messages.findIndex(m => m.pending == true && m._id === data._id);
-            let messages = [...this.state.messages];
-            let sentMessage = {
-              ...messages[sentIndex],
-              pending: false,
-              sent: true
+            if (data.user._id == this.context.user._id) {
+              const sentIndex = this.state.messages.findIndex(
+                (m) => m.pending == true && m._id === data._id
+              );
+              let messages = [...this.state.messages];
+              let sentMessage = {
+                ...messages[sentIndex],
+                pending: false,
+                sent: true,
+              };
+              messages[sentIndex] = sentMessage;
+              this.setState({ messages });
+            } else {
+              this.setState((previousState) => ({
+                messages: GiftedChat.append(this.state.messages, data),
+              }));
             }
-            messages[sentIndex] = sentMessage;
-            this.setState({ messages });
-          } else {
-            this.setState(previousState => ({
-              messages: GiftedChat.append(this.state.messages, data)
-            }));
-          }
+          };
         }
-      }
-    }).catch(e => {
-      console.log(e);
-    });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   }
 
   onSend(messages) {
     messages = messages.map(formatMessage);
-    this.setState(previousState => ({
+    this.setState((previousState) => ({
       messages: GiftedChat.append(previousState.messages, messages),
     }));
 
@@ -101,11 +107,11 @@ class Chat extends React.Component {
       <View style={{ backgroundColor: "white", flex: 1, paddingTop: 20 }}>
         <GiftedChat
           messages={this.state.messages}
-          onSend={(message)=>{
-            this.onSend(message)
+          onSend={(message) => {
+            this.onSend(message);
           }}
           user={{
-            _id: this.context.user._id
+            _id: this.context.user._id,
           }}
         />
       </View>
